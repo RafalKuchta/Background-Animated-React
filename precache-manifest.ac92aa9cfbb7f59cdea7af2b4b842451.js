c09482619bff9ef7b347ef7c80aad86bfc2c9565
Kuchta Rafał <RKuchta@multikino.grupa.iti>
2019-08-16 19:49:28 +0200

    Updates

---
+++ precache-manifest.ac92aa9cfbb7f59cdea7af2b4b842451.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "706dea7318b0b2237f62900bffb6024a",
+    "url": "/RafalKuchta/Background-Animated-React/index.html"
+  },
+  {
+    "revision": "04df4dc59f3b0a320b78",
+    "url": "/RafalKuchta/Background-Animated-React/static/css/main.bfabc67d.chunk.css"
+  },
+  {
+    "revision": "0330f993eb0f0b97875b",
+    "url": "/RafalKuchta/Background-Animated-React/static/js/2.c5b96339.chunk.js"
+  },
+  {
+    "revision": "04df4dc59f3b0a320b78",
+    "url": "/RafalKuchta/Background-Animated-React/static/js/main.8ca7cd16.chunk.js"
+  },
+  {
+    "revision": "6dc9c3d6f7d83589a964",
+    "url": "/RafalKuchta/Background-Animated-React/static/js/runtime~main.d3af4d03.js"
+  }
+]);
