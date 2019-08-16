ad88e29e51c219d6d60629f1ed4415e02cf0472f
Kuchta Rafał <RKuchta@multikino.grupa.iti>
2019-08-16 19:56:59 +0200

    Updates

---
+++ precache-manifest.0a83710a6b237119ae9fe35424da6d80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50eab5e0b724680a5eef11fff6bd28e2",
+    "url": "/Background-Animated-React/index.html"
+  },
+  {
+    "revision": "7ea2ce3e3c9e0ca2c700",
+    "url": "/Background-Animated-React/static/css/main.bfabc67d.chunk.css"
+  },
+  {
+    "revision": "0330f993eb0f0b97875b",
+    "url": "/Background-Animated-React/static/js/2.c5b96339.chunk.js"
+  },
+  {
+    "revision": "7ea2ce3e3c9e0ca2c700",
+    "url": "/Background-Animated-React/static/js/main.e5f19d49.chunk.js"
+  },
+  {
+    "revision": "2d60677ab2fde26e7300",
+    "url": "/Background-Animated-React/static/js/runtime~main.08da3730.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RafalKuchta/Background-Animated-React/precache-manifest.ac92aa9cfbb7f59cdea7af2b4b842451.js"
+  "/Background-Animated-React/precache-manifest.0a83710a6b237119ae9fe35424da6d80.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RafalKuchta/Background-Animated-React/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Background-Animated-React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
